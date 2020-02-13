5a41306d89d50dae0dcfe1cb84c4279d566f8cea
Arktech-SmartHome <58663831+Arktech-SmartHome@users.noreply.github.com>
2020-02-13 11:17:46 +0530

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        /**
+                public string Index()
+                {
+                    return "This is my default action...";
+                }
+        **/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /**
+         * public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    
+    **/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    
+
+        
+    }
+}
